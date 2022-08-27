8832d95ab304d7732a2f0a1f395f9c6827da51f9
Pedro Argentati <pedro.argentatii@gmail.com>
2022-08-27 18:35:52 -0300

    fix: cpf com outOfBound

--- src/br/fiap/modelo/BilheteUnico.java
+++ src/br/fiap/modelo/BilheteUnico.java
@@ -43,7 +43,6 @@ public class BilheteUnico {
 	
 	@Override
 	public String toString() {
-		cpf = (cpf.substring(0, 3) + "." + cpf.substring(3, 6) + "." + cpf.substring(6, 9) + "-" + cpf.substring(9, 11));
-		return "\n Número: " + numero + "\n CPF: " + cpf + "\nSaldo: R$" + saldo;
+		return "\n\n Número: " + numero + "\n CPF: " + cpf + "\nSaldo: R$" + saldo;
 	}
 }
